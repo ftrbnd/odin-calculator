cff79280ff811c8bd9b77696682722a1fb8adf81
ftrbnd <giosalas25@gmail.com>
2023-01-31 15:49:33 -0800

    Finish basic calculations

--- script.js
+++ script.js
@@ -1,3 +1,75 @@
+const buttons = document.querySelectorAll('button');
+const display = document.querySelector('.display');
+let num1 = '', operator = '', num2 = '', startSecondOperand = false;
+
+loadPage();
+
+function loadPage() {
+    attachButtonListeners();
+}
+
+function attachButtonListeners() {
+    for (const button of buttons) {
+        button.addEventListener('mouseover', () => {
+            button.classList.add('darken');
+        });
+    
+        button.addEventListener('mouseout', () => {
+            button.classList.remove('darken');
+        })
+        
+        button.addEventListener('click', () => {
+            handleClick(button.textContent);
+        });
+    }
+}
+
+function handleClick(buttonText) {
+    console.log(`num1 = ${num1}, operator = ${operator}, num2 = ${num2}`);
+
+    if ('+-*÷'.includes(buttonText)) {
+        display.textContent = buttonText;
+        operator = buttonText;
+        startSecondOperand = true;
+        return;
+    }
+
+    if (buttonText == 'CLEAR') {
+        display.textContent = 'Hello';
+        num1 = '', operator = '', num2 = '', startSecondOperand = false;
+        return;
+    } else if (buttonText == '=') {
+        const solution = operate(num1, num2, operator);
+        display.textContent = Math.round(solution * 100) / 100;
+
+        num1 = '', operator = '', num2 = '', startSecondOperand = false;
+        return;
+    }
+
+    if (startSecondOperand) {
+        num2 += buttonText;
+        display.textContent = num2;
+    } else {
+        num1 += buttonText;
+        display.textContent = num1;
+    }
+}
+
+function operate(x, y, op) {
+    x = parseInt(x), y = parseInt(y);
+
+    switch (op) {
+        case '+':
+            return add(x,y);
+        case '-':
+            return subtract(x,y);
+        case '*':
+            return multiply(x,y);
+        case '÷':
+            return divide(x,y);
+    }
+}
+
 function add(x, y) {
     return x + y;
 }
@@ -12,17 +84,4 @@ function multiply(x, y) {
 
 function divide(x, y) {
     return x / y;
-}
-
-function operate(x, y, op) {
-    switch (op) {
-        case '+':
-            return add(x,y);
-        case '-':
-            return subtract(x,y);
-        case '*':
-            return multiply(x,y);
-        case '/':
-            return divide(x,y);
-    }
 }
